ac9e41c4cc7afb48c246f36923d83f46e982d3b9
bvelivala <bhavaniv448@gmail.com>
2017-03-14 18:52:50 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,11 @@ echo "Tomcat Serrver Url is : $TOMCAT_SERVER_URL"
 VERSION_NO=$2
 echo "Version to be downloaded is : $VERSION_NO"
 
+TOMCAT_ADMIN=$3
+echo "Tomcat Admin user id is : $TOMCAT_ADMIN_PASSWD"
+
+TOMCAT_ADMIN_PASSWD=$4
+
 cd /tmp
 
 echo "Downloading autoclaim war version $VERSION_NO"
@@ -18,11 +23,11 @@ if [ -f autoclaim-$VERSION_NO.war ]; then
         echo "Copied autoclaim-$VERSION_NO.war to autoclaim.war"
 
         echo "Undeploy previous version of autoclaim on Tomcat Server $TOMCAT_SERVER_URL"
-        curl -v --basic --user admin:devops "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/undeploy?path=/autoclaim&update=true"
+        curl -v --basic --user $TOMCAT_ADMIN:$TOMCAT_ADMIN_PASSWD "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/undeploy?path=/autoclaim&update=true"
         echo "Undeploy autoclaim on Tomcat Server $TOMCAT_SERVER_URL is successful"
         
         echo "Deploying autoclaim.war to Tomcat Server $TOMCAT_SERVER_URL"
-        curl -v --basic --user admin:devops --upload-file autoclaim.war "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/deploy?path=/autoclaim&update=true"
+        curl -v --basic --user $TOMCAT_ADMIN:$TOMCAT_ADMIN_PASSWD --upload-file autoclaim.war "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/deploy?path=/autoclaim&update=true"
         echo "Deployment of autoclaim app binaries version $VERSION_NO on server $TOMCAT_SERVER_URL is Successful"
         
         rm -f autoclaim*.war
