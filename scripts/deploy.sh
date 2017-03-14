ec324a363d6ca8215ce05db22d923bfe0a90c0e5
bvelivala <bhavaniv448@gmail.com>
2017-03-14 10:55:06 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,11 +17,16 @@ if [ -f autoclaim-$VERSION_NO.war ]; then
         cp autoclaim-$VERSION_NO.war autoclaim.war
         echo "Copied autoclaim-$VERSION_NO.war to autoclaim.war"
 
+        echo "Undeploy previous version of autoclaim on Tomcat Server $TOMCAT_SERVER_URL"
+        curl -v --basic --user admin:devops "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/undeploy?path=/autoclaim&update=true"
+        echo "Undeploy autoclaim on Tomcat Server $TOMCAT_SERVER_URL is successful"
+        
         echo "Deploying autoclaim.war to Tomcat Server $TOMCAT_SERVER_URL"
         curl -v --basic --user admin:devops --upload-file autoclaim.war "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/deploy?path=/autoclaim&update=true"
-        echo "Deployment Successful"
+        echo "Deployment of autoclaim app binaries version $VERSION_NO on server $TOMCAT_SERVER_URL is Successful"
+        
         rm -f autoclaim*.war
         echo "Downloaded binaries deleted from tmp folder"
 else
-        echo "Deployment Failed"
+        echo "Deployment of autoclaim app binaries version $VERSION_NO on server $TOMCAT_SERVER_URL Failed"
 fi
